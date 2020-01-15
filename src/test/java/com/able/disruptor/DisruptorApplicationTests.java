f885347d6e955e5cff00b0a79592883cc0640864
jipeng <jipeng@able-elec.com>
2020-01-15 13:48:55 +0800

    首次提交

---
+++ src/test/java/com/able/disruptor/DisruptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.able.disruptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisruptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
